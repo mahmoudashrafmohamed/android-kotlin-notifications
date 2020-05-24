58d4cfa168f0b03d24764c838430b9f108e06544
dev_mahmoud_ashraf <ashraaf.mahmoud@gmail.com>
2020-05-24 11:04:20 +0200

    test big pic style

---
+++ app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
@@ -0,0 +1,99 @@
+/*
+ * Copyright (C) 2019 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.eggtimernotifications.util
+
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import androidx.core.app.NotificationCompat
+import com.example.android.eggtimernotifications.MainActivity
+import com.example.android.eggtimernotifications.R
+import com.example.android.eggtimernotifications.receiver.SnoozeReceiver
+
+// Notification ID.
+private val NOTIFICATION_ID = 0
+private val REQUEST_CODE = 0
+private val FLAGS = 0
+
+/**
+ * Builds and delivers the notification.
+ *
+ * @param context, activity context.
+ */
+fun NotificationManager.sendNotification(messageBody: String, applicationContext: Context) {
+    // Create the content intent for the notification, which launches
+    // this activity
+    //  create intent
+    val contentIntent = Intent(applicationContext, MainActivity::class.java)
+
+    // create PendingIntent to the system will use it to open the app
+    val contentPendingIntent = PendingIntent.getActivity(
+        applicationContext,
+        NOTIFICATION_ID,
+        contentIntent,
+        PendingIntent.FLAG_UPDATE_CURRENT
+    )
+
+    //  add style
+    val eggImage = BitmapFactory.decodeResource(
+        applicationContext.resources,
+        R.drawable.cooked_egg
+    )
+    val bigPicStyle = NotificationCompat.BigPictureStyle()
+        .bigPicture(eggImage)
+        .bigLargeIcon(null)
+
+
+    // TODO: Step 2.2 add snooze action
+
+    // Build the notification
+    val builder = NotificationCompat.Builder(
+        applicationContext,
+        applicationContext.getString(R.string.egg_notification_channel_id)
+    )
+
+
+    //  set title, text and icon to builder
+        .setSmallIcon(R.drawable.cooked_egg)
+        .setContentTitle(applicationContext.getString(R.string.notification_title))
+        .setContentText(messageBody)
+
+    // set content intent
+        .setContentIntent(contentPendingIntent)
+        .setAutoCancel(true)
+
+        // Step 2.1 add style to builder to test big pic style
+        .setStyle(bigPicStyle)
+        .setLargeIcon(eggImage)
+
+
+
+        // TODO: Step 2.3 add snooze action
+
+        // TODO: Step 2.5 set priority
+
+    // call notify
+    notify(NOTIFICATION_ID, builder.build())
+
+}
+
+// Cancel all notifications
+fun NotificationManager.cancelNotifications() {
+    cancelAll()
+}
